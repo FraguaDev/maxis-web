2cab70e1ed9bc36432eaa324b8e2e98abf734a92
Thargelion <elwindwalker@gmail.com>
2018-01-06 13:13:56 -0300

    Mepongolococonmaterial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MaxHeaderComponent } from './max-header/max-header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaxHeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/max-header/max-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-max-header',
+  templateUrl: './max-header.component.html',
+  styleUrls: ['./max-header.component.css']
+})
+export class MaxHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
