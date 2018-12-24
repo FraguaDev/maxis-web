c41e41f221c2b9364ed98d4dc6d33e58eebcee78
Maximiliano De Pietro <elwindwalker@gmail.com>
2018-12-24 17:19:51 -0300

    Limpieza

--- src/app/pages/max-portada/max-portada.component.ts
+++ src/app/pages/max-portada/max-portada.component.ts
@@ -1,45 +1,43 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MaxTileModel} from '../../components/max-tile/max-tile.component.model';
-import {MaxTypewriterComponent} from '../../components/max-typewriter/max-typewriter.component';
 
 @Component({
-  selector: 'app-max-portada',
-  templateUrl: './max-portada.component.html',
-  styleUrls: ['./max-portada.component.scss']
+    selector: 'app-max-portada',
+    templateUrl: './max-portada.component.html',
+    styleUrls: ['./max-portada.component.scss']
 })
 export class MaxPortadaComponent implements OnInit {
 
-  @Input() maxTypewriter: MaxTypewriterComponent;
 
-  development: MaxTileModel = {
-    title: 'Desarrollo',
-    text: 'Desarrollos :O',
-    style: {
-      width: '100%',
-      height: '100%'
+    development: MaxTileModel = {
+        title: 'Desarrollo',
+        text: 'Desarrollos :O',
+        style: {
+            width: '100%',
+            height: '100%'
+        }
+    };
+
+    curriculumVitae: MaxTileModel = {
+        title: 'CV',
+        text: 'keke',
+        style: {
+            width: '100%',
+            height: '100%'
+        }
+    };
+
+    tiles = [
+        {maxTile: this.development, cols: 2, rows: 2, style: 'url(../assets/img/fl.jpg)', class: 'developer'},
+        {maxTile: this.curriculumVitae, cols: 2, rows: 2, style: 'blue', class: 'developer'},
+        {maxTile: this.development, cols: 2, rows: 2, style: 'url(../assets/img/fl.jpg)', class: 'developer'},
+        {maxTile: this.development, cols: 2, rows: 2, style: 'url(../assets/img/fl.jpg)', class: 'developer'},
+    ];
+
+    constructor() {
     }
-  };
 
-  curriculumVitae: MaxTileModel = {
-    title: 'CV',
-    text: 'keke',
-    style: {
-      width: '100%',
-      height: '100%'
+    ngOnInit() {
     }
-  };
-
-  tiles = [
-    {maxTile: this.development, cols: 2, rows: 2, style: 'url(../assets/img/fl.jpg)', class: 'developer'},
-    {maxTile: this.curriculumVitae, cols: 2, rows: 2, style: 'blue', class: 'developer'},
-    {maxTile: this.development, cols: 2, rows: 2, style: 'url(../assets/img/fl.jpg)', class: 'developer'},
-    {maxTile: this.development, cols: 2, rows: 2, style: 'url(../assets/img/fl.jpg)', class: 'developer'},
-  ];
-
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
 
 }
